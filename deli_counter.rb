42d34dcfce1a61f2a613a189d87d4a7ebda00244
Joe Tustin <j.tustin@gmail.com>
2019-03-13 19:21:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(katz_deli, new_person)
   new_katz_deli << katz_deli
   new_katz_deli << new_person
   ##puts new_katz_deli
-
+  puts new_katz_deli
   puts "Welcome, #{new_person}. You are number #{new_katz_deli.length + 1} in line."
   puts new_katz_deli
 end
