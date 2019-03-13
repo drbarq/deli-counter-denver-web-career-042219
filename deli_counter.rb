ea2abc4c0d71f84fbb1c52791db76cd9907106f1
Joe Tustin <j.tustin@gmail.com>
2019-03-13 18:41:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   end
   if katz_deli.length > 0
     current_line = Array.new
-    katz_deli.each {|person| current_line << "#{katz_deli.index(person)}. name "}
+    katz_deli.each {|person| current_line << "#{katz_deli.index(person)}. person "}
     puts "The line is currently: #{current_line}"
   end
 end
