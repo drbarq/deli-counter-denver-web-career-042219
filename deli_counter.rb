5cfc0d4a4df705fa861bf7e6751c711c3e1efe31
Joe Tustin <j.tustin@gmail.com>
2019-03-13 18:59:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 
-##katz_deli = []
+##katz_deli = ["Logan", "Avi", "Spencer"]
 
 def line(katz_deli)
   if katz_deli.length == 0
@@ -11,6 +11,6 @@ def line(katz_deli)
     katz_deli.each {|person| current_line << (katz_deli.index(person) + 1)}
     ##puts current_line
     puts current_line
-    puts "The line is currently: #{current_line}"
+  ##  puts "The line is currently: #{current_line}"
   end
 end
