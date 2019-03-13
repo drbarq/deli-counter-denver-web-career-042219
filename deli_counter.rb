7c7ffb3344d7f082de902aad7a9f889ea121cc37
Joe Tustin <j.tustin@gmail.com>
2019-03-13 19:00:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
   if katz_deli.length > 0
-    current_line = String.new
-    katz_deli.each {|person| current_line << (katz_deli.index(person) + 1)}
-    ##puts current_line
+    current_line = Array.new
+    katz_deli.each {|person| current_line << "#{katz_deli.index(person) + 1}. #{person}}
     puts current_line
   ##  puts "The line is currently: #{current_line}"
   end
