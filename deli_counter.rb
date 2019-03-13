17a352d2e9ac275b268c40bd6c32937386531fd9
Joe Tustin <j.tustin@gmail.com>
2019-03-13 18:56:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,11 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
   if katz_deli.length > 0
-    current_line = 
+    current_line = []
+    current_line_string = new(str=current_line)
     katz_deli.each {|person| current_line << (katz_deli.index(person) + 1)}
     ##puts current_line
+    
     puts "The line is currently: #{current_line}"
   end
 end
