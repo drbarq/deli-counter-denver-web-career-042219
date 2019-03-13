8dbaaa44497ab3681d198077f1251603b9bce80a
Joe Tustin <j.tustin@gmail.com>
2019-03-13 18:57:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
     current_line = String.new
     katz_deli.each {|person| current_line << (katz_deli.index(person) + 1)}
     ##puts current_line
-    puts current_line_string
+    puts current_line
     puts "The line is currently: #{current_line}"
   end
 end
